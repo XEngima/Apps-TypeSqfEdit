27269dd7b0cbe7be1f136c234d06bf3ba5b40e2b
SCBDEIS <SCBDEIS@CLD10302>
2020-11-09 15:26:50 +0100

    Changed name of menu item 'Browse SQX Reference Page'.

--- TypeSqf.Model/CurrentApplication.cs
+++ TypeSqf.Model/CurrentApplication.cs
@@ -34,7 +34,7 @@ namespace TypeSqf.Model
         {
             get
             {
-                return new AppVersion(1, 06, 04);
+                return new AppVersion(1, 06, 06);
             }
         }
 
