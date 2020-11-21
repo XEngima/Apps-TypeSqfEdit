468dc0c07ec0c0c2d562343f27e68541e0eec3fc
Daniel Eiserman <daniel.eiserman@protonmail.com>
2020-11-21 22:03:45 +0100

    Updated the version number.

--- TypeSqf.Model/CurrentApplication.cs
+++ TypeSqf.Model/CurrentApplication.cs
@@ -34,7 +34,7 @@ namespace TypeSqf.Model
         {
             get
             {
-                return new AppVersion(1, 07, 01);
+                return new AppVersion(1, 07, 10);
             }
         }
 
