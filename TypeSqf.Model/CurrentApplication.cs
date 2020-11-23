aaae1db75733dbae3bde8138130ed684a06b5476
SCBDEIS <SCBDEIS@CLD10302>
2020-11-23 13:47:01 +0100

    Updated version number.

--- TypeSqf.Model/CurrentApplication.cs
+++ TypeSqf.Model/CurrentApplication.cs
@@ -34,7 +34,7 @@ namespace TypeSqf.Model
         {
             get
             {
-                return new AppVersion(1, 07, 10);
+                return new AppVersion(1, 07, 11);
             }
         }
 
